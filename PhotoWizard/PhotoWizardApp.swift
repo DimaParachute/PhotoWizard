3d06c68d19faf006441be69e2b570b8e4adb184f
DimaParachute <dimaparachute11@icloud.com>
2023-05-04 13:08:32 +0300

    Initial Commit

---
+++ PhotoWizard/PhotoWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoWizardApp.swift
+//  PhotoWizard
+//
+//  Created by Дмитрий Фетюхин on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
